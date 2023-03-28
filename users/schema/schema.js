f2aebe1aadbf9a8e2770e083914063d9dece053e
gabrielc42 <clark.gabe97@gmail.com>
2023-03-27 19:53:33 -0500

    async resolve functions

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
-
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
@@ -9,10 +8,6 @@ const {
   GraphQLSchema
 } = graphql;
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 },
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,9 +23,10 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQLString }},
+      args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
       }
     }
   }
