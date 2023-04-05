b93260eaf9a9b390caee3e07e86aa32ce4744edc
gabrielc42 <clark.gabe97@gmail.com>
2023-04-04 19:52:34 -0500

    resolve circular references with closures

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -11,7 +11,7 @@ const {
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
@@ -22,13 +22,13 @@ const CompanyType = new GraphQLObjectType({
           .then(res => res.data);
       }
     }
-  }
+  })
 });
 
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -39,7 +39,7 @@ const UserType = new GraphQLObjectType({
           .then(res => res.data);
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
